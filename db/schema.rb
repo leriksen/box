5f2f07e9a2f1693a4421acc843860b974be533aa
leriksen <leif.eriksen.au@gmail.com>
2014-01-27 18:05:34 +1100

    add locked_at in support of :lockable devise module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109140007) do
+ActiveRecord::Schema.define(version: 20140127065913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140109140007) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "locked_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
