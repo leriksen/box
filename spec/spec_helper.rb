8649a1adb6207b614add0f310afec9e3a9d6e6f1
leriksen <leif.eriksen.au@gmail.com>
2014-02-09 11:40:55 +1100

    more cleanup on spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,14 +11,13 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
+    config.mock_with :rspec
     config.use_transactional_fixtures = true
-
     config.infer_base_class_for_anonymous_controllers = false
-
     config.order = "random"
 
     # Include the Capybara DSL so that specs in spec/requests still work.
-    # config.include Capybara::DSL
+    config.include Capybara::DSL
 
     config.include FactoryGirl::Syntax::Methods
 
@@ -33,11 +32,4 @@ Spork.each_run do
   load "#{Rails.root}/config/routes.rb"
   Dir["#{Rails.root}/app/**/*.rb"].each {|f| load f}
   Dir["#{Rails.root}/lib/**/*.rb"].each {|f| load f}
-
-  # This code will be run each time you run your specs.
-  RSpec.configure do |config|
-    config.mock_with :rspec
-
-    config.use_transactional_fixtures = true
-  end
 end
