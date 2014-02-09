0fd7cd72ac11d5cd1d9f09fc173b651306f095e3
leriksen <leif.eriksen.au@gmail.com>
2014-02-09 20:26:13 +1100

    Found that devise puts some non-standard flash messages in the flashes array - munge these to a basically displayable format

--- app/helpers/foundation_helper.rb
+++ app/helpers/foundation_helper.rb
@@ -6,6 +6,13 @@ module FoundationHelper
 
     content = []
     flashes.each do |kind, message|
+
+      # some of the devise flashes are of the form
+      # :timedout => true
+      # there is always a standard flash message in the array as well,
+      # so we just use the kind for these non-standard ones
+      message = kind.to_s.humanize unless flash_foundation.has_key?(kind)
+
       style = flash_foundation.fetch(kind, 'standard')
 
       fragment = content_tag(:div, message, class: "alert-box radius #{style}", data: {alert: ''}) do
