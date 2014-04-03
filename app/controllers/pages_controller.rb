fbc48269ea15c5c33d025bdabf34a625bf5b2122
leriksen <leif.eriksen.au@gmail.com>
2014-04-03 20:35:54 +1100

    When no users, steal Hobo's idea and redirect from root to the new_user_registration_path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   rescue_from ActionView::MissingTemplate, with: :invalid_page
 
   def index
+    redirect_to(new_user_registration_path,
+                alert: "No users exist yet. Please create Admin user") and return if User.count == 0
   end
 
   def show
