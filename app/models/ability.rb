8f0d8a7b37c6284b4d9d213b7c8fec114e9b7a12
leriksen <leif.eriksen.au@gmail.com>
2014-04-10 22:26:47 +1000

    minor ability updates - will defer further ability definitions until can customise the User save method correctly

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,20 @@
 class Ability
   include CanCan::Ability
   
-  def initialize(user)
+  def initialize(user=nil)
     user ||= User.new # guest user
     
+    # common actions are :read, :create, :update and :destroy
     if user.is? :admin
       can :manage, :all
     elsif user.is? :manager
       # cannot destroy - though we may support an inactive flag and make that the destroy
-      can [:create, :update], :all
+      can [:create, :read, :update], User
+      cannot :destory, :all
     elsif user.is? :staff
       can :update, User
+      cannot :create, User
+      cannot :destroy, :all
     elsif user.is? :customer
     else # guest
     end
