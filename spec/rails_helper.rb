74babc5113df0202051dd402bf276e5471a1c430
leriksen <leif.eriksen.au@gmail.com>
2015-06-03 12:53:25 +1000

    silcence most of th AR:logger noise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,9 @@ Spork.prefork do
 
   require File.expand_path("../../config/environment", __FILE__)
 
+  # turn off noise AR logger - must come before migration check
+  ActiveRecord::Base.logger.level = 1
+
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
