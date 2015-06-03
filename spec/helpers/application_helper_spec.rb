f0c78681c9773bd6ba624222a97e188f91def8f4
leriksen <leif.eriksen.au@gmail.com>
2015-06-03 12:11:52 +1000

    fix error in using config

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe ApplicationHelper do
   it 'returns name from econfig' do
-    expect(Box.site_name).to eq("KennelBox")
+    expect(Box.config.site_name).to eq("KennelBox")
   end
 end
