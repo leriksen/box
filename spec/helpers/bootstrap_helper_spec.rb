a1738f17c7b510ea49426f32cf79ff88fccf4e77
leriksen <leif.eriksen.au@gmail.com>
2015-06-03 12:13:30 +1000

    fix testing of changes introduced with latest bootstrap-rails

--- spec/helpers/bootstrap_helper_spec.rb
+++ spec/helpers/bootstrap_helper_spec.rb
@@ -6,10 +6,10 @@ describe BootstrapHelper do
   end
 
   it 'can format a single known flash message' do
-    expect(helper.flash_map(notice: 'here')).to eq('<div class="alert alert-success alert-dismissable" data-alert="">here<button aria-hidden="true" class="close" data-dismiss="alert" name="button" type="submit">&amp;times;</button></div>')
+    expect(helper.flash_map(notice: 'here')).to eq('<div class="alert alert-success alert-dismissable" data-alert="">here<button name="button" type="submit" class="close" data-dismiss="alert" aria-hidden="true">&amp;times;</button></div>')
   end
   
   it 'can format multiple known flash messages' do
-    expect(helper.flash_map(notice: 'here', error: 'there')).to eq('<div class="alert alert-success alert-dismissable" data-alert="">here<button aria-hidden="true" class="close" data-dismiss="alert" name="button" type="submit">&amp;times;</button></div><div class="alert alert-danger alert-dismissable" data-alert="">there<button aria-hidden="true" class="close" data-dismiss="alert" name="button" type="submit">&amp;times;</button></div>')
+    expect(helper.flash_map(notice: 'here', error: 'there')).to eq('<div class="alert alert-success alert-dismissable" data-alert="">here<button name="button" type="submit" class="close" data-dismiss="alert" aria-hidden="true">&amp;times;</button></div><div class="alert alert-danger alert-dismissable" data-alert="">there<button name="button" type="submit" class="close" data-dismiss="alert" aria-hidden="true">&amp;times;</button></div>')
   end
 end
